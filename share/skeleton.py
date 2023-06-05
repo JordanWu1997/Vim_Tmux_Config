753349bc8e6e54313774dce81939a25caf154c78
JordanWu <jordankhwu@gmail.com>
2023-06-05 21:57:39 +0800

    update:add_python_skeleton

--- share/skeleton.py
+++ share/skeleton.py
@@ -1,18 +1,14 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 """
-[Add module documentation here]
-
-Author: Jordan Kuan-Hsien Wu
-Contact: jordankhwu@gmail.com
-Date: [Add date here]
+[ADD MODULE DOCUMENTATION HERE AND ALSO REMEMBER TO CHANGE DATE IN BANNER]
 
 # ========================================================================== #
 #  _  __   _   _                                          __        ___   _  #
 # | |/ /  | | | |  Author: Jordan Kuan-Hsien Wu           \ \      / / | | | #
 # | ' /   | |_| |  E-mail: jordankhwu@gmail.com            \ \ /\ / /| | | | #
 # | . \   |  _  |  Github: https://github.com/JordanWu1997  \ V  V / | |_| | #
-# |_|\_\  |_| |_|  LinkedIn:                                 \_/\_/   \___/  #
+# |_|\_\  |_| |_|  Date:   Sun Jun 04 2023                   \_/\_/   \___/  #
 #                                                                            #
 # ========================================================================== #
 """
